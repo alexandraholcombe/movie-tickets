ef095bb16c69fe18ca82f2a6a22a8eb6d192dfd0
Alexandra Holcombe and Marc Larkin <alexandra@>
2017-01-24 11:28:17 -0800

    Add Ticket constructor and age variable to JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,17 @@
 //Business
+function Ticket(age, movieTitle, time) {
+  this.age = age;
+  this.movieTitle = movieTitle;
+  this.time = time
+};
+
 
 //User Interface
 $(function(){
   $("form.age-form").submit(function(event) {
+    debugger;
     event.preventDefault();
+    var age = $(".age-select").val();
     $(".age-verification").fadeOut();
     $(".row").delay(400).fadeIn();
   });
